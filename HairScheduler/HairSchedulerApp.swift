bbf63780c52d8edd3e15aa01ce9ae0329649ecd6
Brandon Hua <bhua120605@gmail.com>
2026-09-07 15:43:54 +1000

    feat: add initial Xcode project
    Set up initial iOS project structure and added the default Xcode project files.

---
+++ HairScheduler/HairSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HairSchedulerApp.swift
+//  HairScheduler
+//
+//  Created by Brandon Hua on 7/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct HairSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
